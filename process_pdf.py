1c8edc7e164aed1f26b00b4b14177ccee1f6c5ba
Sieger van Breugel <sieger.van.breugel@fabrique.nl>
2026-03-12 14:40:05 +0100

    Added sanity checks and status prints

--- process_pdf.py
+++ process_pdf.py
@@ -5,7 +5,18 @@ dir = './corpus'
 target_dir = './processed_corpus'
 
 for filename in os.listdir(dir):
-    text = extract_text(os.path.join(dir, filename))
+        if filename.replace("pdf", "txt") in os.listdir(target_dir):
+            print(f"{filename} already found in target directory, skipping.")
+            continue
+
+        print(f"Processing {filename}")
+        try:
+            text = extract_text(os.path.join(dir, filename))                        
+            with open(os.path.join(target_dir, filename.replace("pdf", "txt")), "w") as target_file:
+                target_file.write(text)
+            print("Succes!")
+
+        except:
+            print(f"Error in {filename}, skipping")    
+
     
-    with open(os.path.join(target_dir, filename.replace("pdf", "txt")), "w") as target_file:
-        target_file.write(text)
