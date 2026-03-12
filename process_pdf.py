135a584d8f6efec5b09c63a8f6f716cb2436ad10
Sieger van Breugel <sieger.van.breugel@fabrique.nl>
2026-03-12 16:52:09 +0100

    Removed some bugs

--- process_pdf.py
+++ process_pdf.py
@@ -5,14 +5,16 @@ dir = './corpus'
 target_dir = './processed_corpus'
 
 for filename in os.listdir(dir):
-        if filename.replace("pdf", "txt") in os.listdir(target_dir):
+        new_file_name = filename.replace(".pdf", ".txt").replace("'", "_").replace(" ", "_")
+
+        if new_file_name in os.listdir(target_dir):
             print(f"{filename} already found in target directory, skipping.")
             continue
 
         print(f"Processing {filename}")
         try:
             text = extract_text(os.path.join(dir, filename))                        
-            with open(os.path.join(target_dir, filename.replace("pdf", "txt")), "w") as target_file:
+            with open(os.path.join(target_dir, new_file_name), "w") as target_file:
                 target_file.write(text)
             print("Succes!")
 
